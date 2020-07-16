6ad289faca542a9d6d1051f603bfd0f2aa15c2e2
mark-scholes <innominatus15@outlook.com>
2020-07-16 13:45:45 +0100

    Basic framework of the app is in place.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 
 const App = () => {
-  return <div className="App">Test</div>;
+  const [input, setInput] = useState("");
+
+  return (
+    <div className="App">
+      <h1> FaceBook Messenger Clone</h1>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button type="submit">Send</button>
+    </div>
+  );
 };
 
 export default App;
