687d9abe37b21985f450ffc56b76b7253df24546
mark-scholes <innominatus15@outlook.com>
2020-07-17 12:40:14 +0100

    Fixed an issue whereby the user name of previous messages was being replaced by the logged in user. Also used a sort funcition on the db.collection(messages) obj so messages are sorted by the server timestamp when they are created. At the moment this is being shown in reverse order until I can impliment a way for messages to auto scroll when a new message is added.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,43 @@
 import React, { useState, useEffect } from "react";
 import { Button, FormControl, InputLabel, Input } from "@material-ui/core";
 import Message from "./components/Message";
-
+import db from "./Firebase";
 import "./App.css";
+import firebase from "firebase";
 
 const App = () => {
   const [input, setInput] = useState("");
-  const [messages, setMessages] = useState([
-    { username: "Mark", text: "a" },
-    { username: 2, text: "b" },
-  ]);
-  const [userName, setUserName] = useState("");
-
-  const sendMessage = (e) => {
-    e.preventDefault();
-    setMessages([...messages, { username: userName, text: input }]);
-    setInput("");
-  };
+  const [messages, setMessages] = useState([]);
+  const [username, setUserName] = useState("");
 
   useEffect(() => {
     setUserName(prompt("Enter you name"));
   }, []);
 
+  useEffect(() => {
+    db.collection("messages")
+      //this will be ordered in reverse for now until auto scroll is implimented
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setMessages(snapshot.docs.map((doc) => doc.data()));
+      });
+  }, []);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    db.collection("messages").add({
+      message: input,
+      username: username,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
+    setInput("");
+  };
+
   return (
     <div className="App">
       <h1> FaceBook Messenger Clone</h1>
-      <h2>Welcome {userName}</h2>
+      <h2>Welcome {username}</h2>
       <form>
         <FormControl>
           <InputLabel>message...</InputLabel>
@@ -43,7 +55,7 @@ const App = () => {
       </form>
 
       {messages.map((message) => (
-        <Message userName={userName} message={message} />
+        <Message username={username} message={message} />
       ))}
     </div>
   );
--- src/components/Message.js
+++ src/components/Message.js
@@ -2,14 +2,15 @@ import React from "react";
 import { CardContent, Card, Typography } from "@material-ui/core";
 import "../../src/Message.css";
 
-const Message = ({ message, userName }) => {
-  const isUser = userName === message.username;
+const Message = ({ username, message }) => {
+  const isUser = username === message.username;
+
   return (
     <div className={`message_card ${isUser && "message_user"} `}>
       <Card className={isUser ? "message__userCard" : "message__guestCard"}>
         <CardContent>
-          <Typography color="white" variant="h5" component="h2">
-            {message.username} : {message.text}
+          <Typography variant="h5" component="h2">
+            {message.username} : {message.message}
           </Typography>
         </CardContent>
       </Card>
